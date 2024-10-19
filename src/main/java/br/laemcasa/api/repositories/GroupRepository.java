4f8b41810d560eba8337849284f3f104a4754bf5
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2024-10-18 23:06:53 -0300

    feat: create UserRepository, GroupRepository and TaskRepository

---
+++ src/main/java/br/laemcasa/api/repositories/GroupRepository.java
@@ -0,0 +1,13 @@
+package br.laemcasa.api.repositories;
+
+import br.laemcasa.api.domain.group.Group;
+import br.laemcasa.api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+import java.util.UUID;
+
+public interface GroupRepository extends JpaRepository<Group, UUID> {
+    Set<Group> findGroupByOwner(User Owner);
+    Group findGroupByLink(String link);
+}
---
+++ src/main/java/br/laemcasa/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.laemcasa.api.repositories;
+
+import br.laemcasa.api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
